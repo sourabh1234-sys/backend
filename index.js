cd777f80d48b0cefe2ee0d08e0745bcef754d78f
Sourabh <72642622+sourabh1234-sys@users.noreply.github.com>
2024-11-14 13:53:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -38,10 +38,18 @@ app.use(session({
     }
 }));
 
+const allowedOrigins = [process.env.CLIENT_ORIGIN, 'https://frontend-gd50.onrender.com'];
 app.use(cors({
-    origin: process.env.CLIENT_ORIGIN,
-    credentials: true, 
+    origin: (origin, callback) => {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
 }));
+
 
 app.use(cookieParser());
 app.use(express.json());
